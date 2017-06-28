d3c6185841e3126584ef6fc765e486a96bb7cbd3
akemflem1 <qhsghks6465@hanmail.net>
2017-06-28 13:37:37 +0900

    ...

---
+++ day6/exam7/exam7.cpp
@@ -0,0 +1,20 @@
+// exam7.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int i;
+
+	for (i = 0; i < 10; i++) {
+		printf("%d \n", i);
+	}
+
+	for (i = 0; i <= 10; i++) {
+		printf("%d \n", i);
+	}
+    return 0;
+}
+
