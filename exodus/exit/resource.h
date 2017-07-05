1e3e4a5ffef73e046fddcf386850dfae9afd75e6
akemflem1 <qhsghks6465@hanmail.net>
2017-07-05 17:50:49 +0900

    ...

---
+++ exodus/exit/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// exit.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define IDD_EXIT_DIALOG                 102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_EXIT                        107
+#define IDI_SMALL                       108
+#define IDC_EXIT                        109
+#define IDR_MAINFRAME                   128
+#define ID_32771                        32771
+#define IDM_START                       32772
+#define ID_32773                        32773
+#define IDM_TILE                        32774
+#define IDM_GET_TILE                    32775
+#define ID_32776                        32776
+#define IDM_GET_TILE123                 32777
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32778
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
