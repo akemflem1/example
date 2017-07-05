26cd1c709bba88855e0e1b3aeaf5c87ff163937f
akemflem1 <qhsghks6465@hanmail.net>
2017-07-05 17:49:48 +0900

    ...

---
+++ exodus/chapter1/mygdiplus_game.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+namespace myGdiplusGame {
+
+	const int g_nTileSize = 16;
+	const int g_nTileXCount = 8;
+
+	void drawTile(Graphics *pGrp, Image *pImgBasicTile, int nPosX, int nPosY, int *pMap)
+	{
+		int nTileIndex = pMap[nPosX + nPosY*g_nTileXCount];
+
+		pGrp->DrawImage(pImgBasicTile, Rect(nPosX * 16, nPosY * 16, g_nTileSize, g_nTileSize),
+			g_nTileSize * (nTileIndex % g_nTileXCount), g_nTileSize * (nTileIndex / g_nTileXCount), g_nTileSize, g_nTileSize, UnitPixel);
+	}
+
+}
